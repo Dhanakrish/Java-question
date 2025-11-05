cf455212043ac53995e3dfe2f094e6394c9d0176
Dhanakrish <dhanashekarcse@gmail.com>
2025-11-05 20:52:36 +0530

    Binarycode

--- src/DecimalToBinary.java
+++ src/DecimalToBinary.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class DecimalToBinary {
+    public static void main(String[] args) {
+       /*  int decimal = 25;
+        String binary = Integer.toBinaryString(decimal);
+        System.out.println("Decimal: " + decimal);
+        System.out.println("Binary: " + binary);*/
+        int decimal = 25;
+        String binary="";
+        while(decimal>0){
+            int remainder=decimal%2;
+            binary=remainder+binary;
+            decimal=decimal/2;
+        }
+        System.out.println("Binary: " + binary);
+    }
 }
